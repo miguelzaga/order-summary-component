dbf4de47e4f636cafcfb8c43cbdc9dbdf1d63c3f
Miguel <miguezaga2@hotmail.com>
2022-08-03 14:37:23 -0500

    style top card and plan object

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,19 +9,23 @@ function Card() {
         src={hero}
         alt="person with headphones dancing"
       ></img>
-      <h1 className="card__title">Order Summary </h1>
-      <p className="card__description">
-        You can now listen to millions of songs, audiobooks, and podcasts on any
-        device anywhere you like!{" "}
-      </p>
-      <div className="card__plan plan">
-        <img src={iconMusic} alt="" className="plan__icon"></img>
-        <h2 className="plan__name">Annual Plan</h2>
-        <p className="plan__price"> $59.99/year</p>
-        <button className="plan__btn-change">Change</button>
+      <div className="card__container">
+        <h1 className="card__title">Order Summary </h1>
+        <p className="card__description">
+          You can now listen to millions of songs, audiobooks, and podcasts on
+          any device anywhere you like!{" "}
+        </p>
+        <div className="card__plan plan">
+          <img src={iconMusic} alt="" className="plan__icon"></img>
+          <div className="plan__text">
+            <h2 className="plan__name">Annual Plan</h2>
+            <p className="plan__price"> $59.99/year</p>
+          </div>
+          <button className="plan__btn-change">Change</button>
+        </div>
+        <button className="card__btn">Proceed to Payment</button>
+        <button className="card__btn-cancel">Cancel Order</button>
       </div>
-      <button className="card__btn">Proceed to Payment</button>
-      <button className="card__btn-cancel">Cancel Order</button>
     </section>
   );
 }
